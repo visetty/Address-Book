d41d66af9cd18761e5e9c5372550a8e1cc6ac036
visetty <visalalakkimsetty@gmail.com>
2018-06-30 00:31:31 -0700

    newcontact.full name is appended in ul and not firstname

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
